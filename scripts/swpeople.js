d2c92d4b4d4c0ade24e5f05a5e4fb8aff6c8b012
Jago <jagovanbeek@gmail.com>
2019-03-05 12:02:08 -0700

    Got basic grid of eye color

--- scripts/swpeople.js
+++ scripts/swpeople.js
@@ -5,7 +5,7 @@ const men = people.filter(person => person.gender == 'male')
 const women = people.filter(person => person.gender == 'female')
 const other = people.filter(person => (person.gender == 'n/a') || (person.gender == 'hermaphrodite') || (person.gender == 'none'))
 
-console.log(men, women, other)
+// console.log(men, women, other)
 
 const mainContainer = document.createElement('div')
 mainContainer.className = 'container'
@@ -14,6 +14,9 @@ men.forEach((man) => {
     let manElement = document.createElement('div')
     manElement.className = 'box'
     manElement.textContent = man.name
+    let eyeColor = document.createElement('p')
+    eyeColor.textContent = man.eye_color
+    manElement.appendChild(eyeColor)
     mainContainer.appendChild(manElement)
 })
 

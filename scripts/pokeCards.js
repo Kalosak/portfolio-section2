01099826641fedb4e287e2007c2cdb9088d1f5d7
Jago <jagovanbeek@gmail.com>
2019-03-28 12:53:38 -0600

    Got fetchSinglePokemon all working

--- scripts/pokeCards.js
+++ scripts/pokeCards.js
@@ -1,13 +1,10 @@
 import { pokemon } from '../scripts/pokemon.js'
 
 class Pokemon {
-  constructor(id) {
-    this.id = id
-    // this.name = name
-  }
+    constructor(id) {
+        this.id = id
+    }
 }
-
-console.log(pokemon)
 
 const mainContainer = document.querySelector('.container')
 
@@ -15,30 +12,22 @@ function createPokeCard(pokeData) {
     let card = document.createElement('div')
     card.className = 'box'
     let figure = document.createElement('figure')
-    let figcap = document.createElement('figcaption')
-    // let title = document.createElement('h2')
+    let caption = document.createElement('figcaption')
     let image = document.createElement('img')
 
-    let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
-    // caption.textContent = upperName
+    //let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
     caption.textContent = pokeData.name
-    if(pokeData.id !=0) {
-      image.src = `../images/$(pokeData.id/$(upperName).png`
+    if(pokeData.id !== 0) {
+        image.src = `../images/${pokeData.imageID}${pokeData.name}.png`
     } else {
-      image.src
+        image.src = `../images/pokeball.png`
     }
-
-    // title.textContent = upperName
-    image.src = pokeData.sprites.front_shiny
-    card.appendChild(title)
-    card.appendChild(image)
+    
+    figure.appendChild(image)
+    figure.appendChild(caption)
+    card.appendChild(figure)
     mainContainer.appendChild(card)
 }
-
-  figure.appendChild(image)
-  figure.appendChild(caption)
-  card.appendChild(figure)
-  mainContainer.appendChild(card)
 
 // pokemon.forEach(singleMon => {
 //     fetch(singleMon.url)
@@ -51,27 +40,33 @@ function createPokeCard(pokeData) {
 // })
 
 function fetchSinglePokemon(id) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-  // .then(response => response.json())
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(retrievedPokemon) {
-    retrievedPokemon.name = retrievedPokemon.name.charAt(0).toUpperCase() + retrievedPokemon.name.slice(1)
-    createPokeCard(retrievedPokemon)
-    // return myJson
-  })
-  // .then(myJson => createPokeCard(myJson))
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(retrievedPokemon) {
+        console.log(typeof(retrievedPokemon.id))
+        if(retrievedPokemon.id < 10) {
+            retrievedPokemon.imageID = "00" + retrievedPokemon.id
+        }
+        if(retrievedPokemon.id > 9 && retrievedPokemon.id < 100 ) {
+            retrievedPokemon.imageID = "0" + retrievedPokemon.id
+        }
+        if(retrievedPokemon.id > 99) {
+            retrievedPokemon.imageID = retrievedPokemon.id
+        }
+        retrievedPokemon.name = retrievedPokemon.name.charAt(0).toUpperCase() + retrievedPokemon.name.slice(1)
+        createPokeCard(retrievedPokemon)
+    })
 }
+
 
 const newPokemonButton = document.querySelector('button')
 
 newPokemonButton.addEventListener('click', function() {
-  let pokemonID = prompt('Enter an ID of an existing Pokemon:')
-  const retrievePokemon = fetchSinglePokemon(pokemonID)
-  // fetchSinglePokemon(pokemonID)
-  // if(pokemonID.length === 1) {
-  //   pokemonID = "00" + pokemonID
-  // }
-  // createPokeCard(new Pokemon(pokemonID))
-})
+    let pokemonID = prompt('Enter an ID of an existing pokemon:')
+    fetchSinglePokemon(pokemonID)
+    if (pokemonID.length ===1){
+      pokemonID ="00" +pokemonID
+    }
+  });

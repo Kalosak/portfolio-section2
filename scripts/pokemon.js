ad65773aedc17081a1b6764edce0c086a6efc512
Jago <jagovanbeek@gmail.com>
2019-04-04 12:36:33 -0600

    Finished assignment before touches

--- scripts/pokemon.js
+++ scripts/pokemon.js
@@ -1,102 +1,102 @@
 export const pokemon = [
-  {
-    name: "mewtwo",
-    url: "https://pokeapi.co/api/v2/pokemon/150/"
-  },
-  {
-    name: "mew",
-    url: "https://pokeapi.co/api/v2/pokemon/151/"
-  },
-  {
-    name: "chikorita",
-    url: "https://pokeapi.co/api/v2/pokemon/152/"
-  },
-  {
-    name: "bayleef",
-    url: "https://pokeapi.co/api/v2/pokemon/153/"
-  },
-  {
-    name: "meganium",
-    url: "https://pokeapi.co/api/v2/pokemon/154/"
-  },
-  {
-    name: "cyndaquil",
-    url: "https://pokeapi.co/api/v2/pokemon/155/"
-  },
-  {
-    name: "quilava",
-    url: "https://pokeapi.co/api/v2/pokemon/156/"
-  },
-  {
-    name: "typhlosion",
-    url: "https://pokeapi.co/api/v2/pokemon/157/"
-  },
-  {
-    name: "totodile",
-    url: "https://pokeapi.co/api/v2/pokemon/158/"
-  },
-  {
-    name: "croconaw",
-    url: "https://pokeapi.co/api/v2/pokemon/159/"
-  },
-  {
-    name: "feraligatr",
-    url: "https://pokeapi.co/api/v2/pokemon/160/"
-  },
-  {
-    name: "sentret",
-    url: "https://pokeapi.co/api/v2/pokemon/161/"
-  },
-  {
-    name: "furret",
-    url: "https://pokeapi.co/api/v2/pokemon/162/"
-  },
-  {
-    name: "hoothoot",
-    url: "https://pokeapi.co/api/v2/pokemon/163/"
-  },
-  {
-    name: "noctowl",
-    url: "https://pokeapi.co/api/v2/pokemon/164/"
-  },
-  {
-    name: "ledyba",
-    url: "https://pokeapi.co/api/v2/pokemon/165/"
-  },
-  {
-    name: "ledian",
-    url: "https://pokeapi.co/api/v2/pokemon/166/"
-  },
-  {
-    name: "spinarak",
-    url: "https://pokeapi.co/api/v2/pokemon/167/"
-  },
-  {
-    name: "ariados",
-    url: "https://pokeapi.co/api/v2/pokemon/168/"
-  },
-  {
-    name: "crobat",
-    url: "https://pokeapi.co/api/v2/pokemon/169/"
-  },
-  {
-    name: "chinchou",
-    url: "https://pokeapi.co/api/v2/pokemon/170/"
-  },
-  {
-    name: "lanturn",
-    url: "https://pokeapi.co/api/v2/pokemon/171/"
-  },
-  {
-    name: "pichu",
-    url: "https://pokeapi.co/api/v2/pokemon/172/"
-  },
-  {
-    name: "cleffa",
-    url: "https://pokeapi.co/api/v2/pokemon/173/"
-  },
-  {
-    name: "igglybuff",
-    url: "https://pokeapi.co/api/v2/pokemon/174/"
-  }
-];
+{
+  "name": "wynaut",
+  "url": "https://pokeapi.co/api/v2/pokemon/360/"
+},
+{
+  "name": "snorunt",
+  "url": "https://pokeapi.co/api/v2/pokemon/361/"
+},
+{
+  "name": "glalie",
+  "url": "https://pokeapi.co/api/v2/pokemon/362/"
+},
+{
+  "name": "spheal",
+  "url": "https://pokeapi.co/api/v2/pokemon/363/"
+},
+{
+  "name": "sealeo",
+  "url": "https://pokeapi.co/api/v2/pokemon/364/"
+},
+{
+  "name": "walrein",
+  "url": "https://pokeapi.co/api/v2/pokemon/365/"
+},
+{
+  "name": "clamperl",
+  "url": "https://pokeapi.co/api/v2/pokemon/366/"
+},
+{
+  "name": "huntail",
+  "url": "https://pokeapi.co/api/v2/pokemon/367/"
+},
+{
+  "name": "gorebyss",
+  "url": "https://pokeapi.co/api/v2/pokemon/368/"
+},
+{
+  "name": "relicanth",
+  "url": "https://pokeapi.co/api/v2/pokemon/369/"
+},
+{
+  "name": "luvdisc",
+  "url": "https://pokeapi.co/api/v2/pokemon/370/"
+},
+{
+  "name": "bagon",
+  "url": "https://pokeapi.co/api/v2/pokemon/371/"
+},
+{
+  "name": "shelgon",
+  "url": "https://pokeapi.co/api/v2/pokemon/372/"
+},
+{
+  "name": "salamence",
+  "url": "https://pokeapi.co/api/v2/pokemon/373/"
+},
+{
+  "name": "beldum",
+  "url": "https://pokeapi.co/api/v2/pokemon/374/"
+},
+{
+  "name": "metang",
+  "url": "https://pokeapi.co/api/v2/pokemon/375/"
+},
+{
+  "name": "metagross",
+  "url": "https://pokeapi.co/api/v2/pokemon/376/"
+},
+{
+  "name": "regirock",
+  "url": "https://pokeapi.co/api/v2/pokemon/377/"
+},
+{
+  "name": "regice",
+  "url": "https://pokeapi.co/api/v2/pokemon/378/"
+},
+{
+  "name": "registeel",
+  "url": "https://pokeapi.co/api/v2/pokemon/379/"
+},
+{
+  "name": "latias",
+  "url": "https://pokeapi.co/api/v2/pokemon/380/"
+},
+{
+  "name": "latios",
+  "url": "https://pokeapi.co/api/v2/pokemon/381/"
+},
+{
+  "name": "kyogre",
+  "url": "https://pokeapi.co/api/v2/pokemon/382/"
+},
+{
+  "name": "groudon",
+  "url": "https://pokeapi.co/api/v2/pokemon/383/"
+},
+{
+  "name": "rayquaza",
+  "url": "https://pokeapi.co/api/v2/pokemon/384/"
+}
+]
